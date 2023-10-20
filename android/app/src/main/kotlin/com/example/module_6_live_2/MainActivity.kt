915fa9f2b0d0d698f7dc99d04b498c4b84e54dc7
syfulsharif <sharif.syful@gmail.com>
2023-10-20 12:27:37 +0600

    repo created

---
+++ android/app/src/main/kotlin/com/example/module_6_live_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.module_6_live_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Module 6: Live 2'),
+        centerTitle: true,
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
