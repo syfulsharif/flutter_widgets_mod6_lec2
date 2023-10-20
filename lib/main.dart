33592bcd6a1765f39f912343b20a44e694aaa647
syfulsharif <sharif.syful@gmail.com>
2023-10-20 16:55:08 +0600

    2nd textEditController added

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,10 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   final TextEditingController _emailTextEditingController =
       TextEditingController();
+  final TextEditingController _userNameTextEditingController =
+  TextEditingController();
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +40,26 @@ class HomeScreen extends StatelessWidget {
               padding: EdgeInsets.all(16.0),
               child: TextField(
                 controller: _emailTextEditingController,
+                decoration: InputDecoration(
+                  label: Text('Email'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _userNameTextEditingController,
+                decoration: InputDecoration(
+                  label: Text('User Name'),
+                ),
               ),
             ),
             ElevatedButton(
               onPressed: () {
                 print(_emailTextEditingController.text);
+                print(_userNameTextEditingController.text);
+                _emailTextEditingController.clear();
+                _userNameTextEditingController.clear();
               },
               child: Text('Show'),
             )
